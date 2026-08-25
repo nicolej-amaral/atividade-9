371002294c4c7bec5866958a552a9ac2e0ca76df
nicole-amaral <nicole_amaral@estudante.sesisenai.org.br>
2026-08-25 08:36:31 -0300

    add inicial commit

---
+++ database/db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE atividade_09;
+
+USE atividade_09;
+
+CREATE TABLE clientes (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    numero VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE  animais(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    descricao TEXT,
+    clientes_id INT NOT NULL,
+
+    FOREIGN KEY (clientes_id)
+    REFERENCES clientes(id)
+);
+
